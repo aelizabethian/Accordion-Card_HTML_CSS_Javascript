8e331829be0bc7c63bf84973cd0713e040573a4c
aelizabethian <amanda.richardson2@gmail.com>
2021-11-05 10:23:24 -0400

    Add updates
    updated CSS, added functionality

---
+++ script.js
@@ -0,0 +1,41 @@
+"use strict";
+
+const questions = document.querySelectorAll(".question");
+const arrows = document.querySelectorAll(".arrow");
+const answers = document.querySelectorAll(".answer");
+
+questions.forEach((question) => {
+  question.addEventListener("click", (event) => {
+    const target = event.target;
+    const closestArrow = event.target.nextElementSibling;
+    function toggleA() {
+      closestArrow.classList.toggle("up");
+      if (closestArrow.classList.contains("up")) {
+        target.style.fontWeight = "bold";
+      } else {
+        target.style.fontWeight = "normal";
+      }
+    }
+
+    if (target.className === "question q0") {
+      document.querySelector(".answer.q0").classList.toggle("hidden");
+      toggleA();
+    }
+    if (target.className === "question q1") {
+      document.querySelector(".answer.q1").classList.toggle("hidden");
+      toggleA();
+    }
+    if (target.className === "question q2") {
+      document.querySelector(".answer.q2").classList.toggle("hidden");
+      toggleA();
+    }
+    if (target.className === "question q3") {
+      document.querySelector(".answer.q3").classList.toggle("hidden");
+      toggleA();
+    }
+    if (target.className === "question q4") {
+      document.querySelector(".answer.q4").classList.toggle("hidden");
+      toggleA();
+    }
+  });
+});
